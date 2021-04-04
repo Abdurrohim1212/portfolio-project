0b6cfa516d33fd85933698b4971cb495add78e29
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-04-04 07:38:12 +0700

    header completed

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typed from "react-typed";
+
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Web Development and Web Promotion</h1>
+        <Typed
+          strings={["Web Design", "Web Development"]}
+          className="typed-text"
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
